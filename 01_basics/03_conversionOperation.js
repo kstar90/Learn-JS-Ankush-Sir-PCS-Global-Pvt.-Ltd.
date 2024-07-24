6c7c659e91f6bba3580899fe39dfd7e4340e7234
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-24 14:09:12 +0530

    Updated
    Complete Investigation study upon Convertion Operation topic with details

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = "";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,5 +28,43 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *** Operations ***
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Ankush";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// num1 = num2 = num3 = 2 + 2;  // very bad practice
+// console.log(+true);  // very bad practice
+// console.log(+""); => 0;
+// console.log(true);
+
+// let num1, num2, num3;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
