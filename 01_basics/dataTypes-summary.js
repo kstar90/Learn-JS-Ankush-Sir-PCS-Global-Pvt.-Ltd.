8fee8793f69d40fb379065bddc2d0effc1cbc7f1
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-24 20:54:14 +0530

    Stack & Heap
    Investigation about stack and heap in details, stack gives copy of the data to use and heap gives the reference data so if we change child data origin/parent data also will be changed in heap

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -14,7 +14,7 @@ const id = Symbol('123');
 const anotherId = Symbol('123');
 
 console.log(id === anotherId); // => false
-// console.log(id); => Symbol('123')
+console.log(typeof id); // => Symbol('123')
 // console.log(anotherId); => Symbol('123')
 
 // const bigNumber = 3456543576654356754n;
@@ -34,6 +34,31 @@ const myFunction = function(){
     console.log(`Hello World!`);
 }
 
-console.log(typeof bigNumber);
-console.log(typeof heros);
-console.log(typeof myFunction);
+// console.log(typeof bigNumber);
+// console.log(typeof heros);
+// console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3 (study material)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive DataTypes), Heap(Reference/Non-Primitive DataTypes)
+
+let myYoutubeName = "ankushadhikari@g.com";
+let anotherName = myYoutubeName;
+anotherName = "chai-aur-code";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "ankush@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
