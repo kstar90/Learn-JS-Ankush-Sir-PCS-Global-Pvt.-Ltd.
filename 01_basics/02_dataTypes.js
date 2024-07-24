029c6c64f722f65a0ad07ea6287a2f13b606e321
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-24 12:47:58 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); (we are using nodejs, not browser)
+
+console.log(3+3); // code readability should be high
+console.log("Ankush");
+
+let name = "Ankush";
+let age = 19;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to the power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => stanalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Ankush");
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
