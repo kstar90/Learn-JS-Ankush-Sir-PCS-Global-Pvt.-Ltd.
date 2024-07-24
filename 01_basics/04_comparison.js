e83ec4a3bd3caa67322976d61b6e22cd9b7f0801
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-24 14:32:49 +0530

    Comparion
    a detailed investigation study what to do and what not to do about comparion in JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // => false
+// console.log(null == 0); // => false
+// console.log(null >= 0);  // => true
+
+// console.log(undefined); => undefined
+// console.log(undefined == 0); // => false
+// console.log(undefined > 0); // => false
+// console.log(undefined < 0); // => false
+
+// === 
+
+// Avoid these type of comparison
+
+console.log("2" === 2); // => false
