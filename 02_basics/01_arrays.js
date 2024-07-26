299691ca2b92359858d6b5de59da5f7852283128
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-26 20:44:50 +0530

    Starting Array
    Starting the Investigation study about arrays in JavaScript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+// array
+
+// array is a object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+
+// The array in JavaScript are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead)
+
+// JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexs, but must be accessed using nonnegative integers (or their respective string form) as indexes
+
+// JavaScript arrays are zero-indexed; the first element of an array is at index 0; the second is at index 1; and so on - and the last element is the value of the array's length property minus 1.
+
+// JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+
+// A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may also cause the other object to change too --- and so, you may end up unintentionally causing changes to the source or copy that you do not expect. That behavior contrasts with the behavior of a deep copy, in which the source and copy are completely independent.
+
+// const myArr = [0, 1, 2, 3, 4, 5, true, "hitesh"];
+const myArr = [0, 1, 2, 3, 4, 5];
+
+console.log(myArr[0]);
