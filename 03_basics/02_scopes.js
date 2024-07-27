553752f956528a9d0fa8519bd59c9176f983e705
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-28 01:59:54 +0530

    Scopes & Hoisting
    Studied about scopes in depth and also did Investigation study about expression and hoisting in JavaScript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,3 +15,47 @@ if(true) {
 console.log(a); // => global scope
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "hitesh";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+        function three(){
+            console.log(website);
+        }
+        three();
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+if(true){
+    const username = "ankush";
+    if(username === "ankush"){
+        const website = " linkedIn";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++
+
+
+// addOne(5);
+console.log(addOne(5));  // => expected output: 6
+function addOne(num){
+    return num + 1;
+}
+
+// hoisting in javascript
+
+// addTwo(5);
+
+const addTwo = function(num){  // Expression sometimes, which is also a function
+    return num + 2;
+}
+// addTwo(5);
+console.log(addTwo(5));  // => expected output: 7
