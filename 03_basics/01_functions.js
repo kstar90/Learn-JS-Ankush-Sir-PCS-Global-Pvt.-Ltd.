8da33310ee64e780664a78b776d418f0ec3a13ec
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-27 14:26:44 +0530

    Function Updated
    Detailed study about function syntaxes in JavaScript with arrays and objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,3 +35,34 @@ function loginUserMessage(username = "sam"){
 // loginUserMessage("Ankush");
 // console.log(loginUserMessage("Ankush"));
 console.log(loginUserMessage(`Ankush`));
+
+function calculateCartPrice(val1, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 1200));
+
+const user = {
+    username: "hitesh",
+    prices: 199,
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399,
+});
+
+
+const myNewArray = [200, 400, 100, 600];
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));  // => expected output: 400
+
+console.log(returnSecondValue([200, 400, 100, 600]));
