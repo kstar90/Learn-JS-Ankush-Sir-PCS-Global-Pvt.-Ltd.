2d2dcdc121218937a44978993686f8c78196fd7a
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-28 16:21:43 +0530

    Control Flow
    Studied about conditional loops and switch case and break and also discussed about truthy values and falsy values also Nullish Coalescing Operator (??) and Ternary Operator in JavaScript

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,68 @@
+// if
+
+// const isUserloggedIn = true;
+
+// if (isUserloggedIn) {}
+
+// if (condition) {}
+
+// if (true) {}
+
+// if (false) {}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// if(2 == "2") {
+//     console.log("executed");
+// }
+
+// if(2 === "2") {
+//     console.log("executed");
+// }
+
+// if( 2 !== 3 ) {
+//     console.log("executed");
+// }
+
+// const temperature = 41;
+
+// if( temperature < 50 ) {
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200;
+
+// if (score > 100) {
+//     let power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000;
+
+// if (balance > 500) console.log("test"), console.log("test 2"); // => bad practice
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2 == 1) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
