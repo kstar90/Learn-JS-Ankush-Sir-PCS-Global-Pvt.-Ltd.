812cf8b59eeaeb4458013e0cff7f4e27b81f4a74
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-28 21:37:43 +0530

    Do While Loop
    Understand the concept of loops, forLoops speacially and also whileLoops and doWhileLoops in details

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// While Loop
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(`Value of index is: ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['flash', 'batman', 'superman', 'wonder woman'];
+
+let arr = 0;
+while ( arr < myArray.length ) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+// Do while Loop
+
+// do {
+    
+// } while (condition);
+
+// let score = 11;
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
