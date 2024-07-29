13d56268ae2440e78bf0f583ef9d5381ff918e2a
Ankush Adhikari <ankushkol35@gmail.com>
2024-07-29 23:29:08 +0530

    Special Loops
    Deep learning, usages and Investigated study about map, filter, reduce, forEach loops ...push, etc in details

---
+++ 05_iterations/seven.js
@@ -0,0 +1,19 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNumbers.map( (num) => num + 10 );
+
+// const newNums = myNumbers.map( (num) => {return num + 10} );
+
+const myNums = [];
+
+myNumbers.forEach( (num) => {
+    num = num + 10;
+    return myNums.push(num);
+})
+
+const newNums = myNumbers
+    .map( (num) => num * 10 )
+    .map( (num) => num + 1 )
+    .filter( (num) => num >= 40 );
+
+console.log(newNums);
