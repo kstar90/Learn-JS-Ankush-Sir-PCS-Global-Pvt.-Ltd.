542f401b54bce209d850d7296fcd2975e787421c
Ankush Adhikari <ankushkol35@gmail.com>
2024-08-09 13:57:01 +0530

    Getter & Setter in JS
    Add getters and setters for improved property management and validation. Custom access and update logic enhances control over object properties

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,20 @@
+// Object 
+
+const User = {
+    _email: 'h@hc.com',
+    _password: 'abc',
+
+    get email(){
+        return this._email.toUpperCase();
+    },
+
+    set email(value){
+        this._email = value;
+    }
+}
+
+// factory function =>
+    
+const tea = Object.create(User);
+
+console.log(tea.email);
